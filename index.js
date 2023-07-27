683682bbb612c142a4bd6abb122f9cd4cb1d9ba1
kyan0045 <84374752+kyan0045@users.noreply.github.com>
2023-07-27 23:17:22 +0200

    v1.1.1 - Add checkRarity

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const solveHint = require('./functions/solveHint.js');
+const checkRarity = require('./functions/checkRarity.js');
 
-const client = { solveHint }
+const client = { solveHint, checkRarity }
 
 module.exports = client;
