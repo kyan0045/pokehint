2e5967d176c2903ec7789485cfeafd4e322748a9
JensenVanStichel <32330303+JensenVanStichel@users.noreply.github.com>
2023-08-18 19:09:37 +0200

    Added regional and event categories
    regional.json and event.json:
    Added regional.json and event.json for regional and event pokemon respectively.
    
    checkRarity.js:
    Added regional and event
    
    pokemon.json:
    Added the latest anniversary pokemons, skater wooper, anniversary lapras and cupcake alcremie.
    
    Fixed Inkay: removed the leading space
    
    Fixed Farfetch'd: Changed the ' to the correct kind, similar to the one for Galarian Farfetch'd
    
    Added Alolan pokemons in the list.
    
    Consistent spacing.

--- functions/checkRarity.js
+++ functions/checkRarity.js
@@ -1,15 +1,21 @@
 async function checkRarity(pokemonName) {
-    const pokemon = require('../data/pokemon.json');
-    const legendaries = require('../data/legendary.json');
-    const mythicals = require('../data/mythical.json');
-    const ultra_beasts = require('../data/ultra-beast.json');
+  const pokemon = require("../data/pokemon.json");
+  const legendaries = require("../data/legendary.json");
+  const mythicals = require("../data/mythical.json");
+  const ultra_beasts = require("../data/ultra-beast.json");
+  const regional = require("../data/regional.json");
+  const event = require("../data/event.json");
 
-    if (!pokemon.includes(pokemonName)) { throw new Error('Unable to identify that pokemon.')}
-    if (legendaries.includes(pokemonName)) return 'legendary'
-    if (mythicals.includes(pokemonName)) return 'mythical'
-    if (ultra_beasts.includes(pokemonName)) return 'ultra_beast'
-    
-    if (pokemon.includes(pokemonName)) return 'regular'
+  if (!pokemon.includes(pokemonName)) {
+    throw new Error("Unable to identify that pokemon.");
+  }
+  if (legendaries.includes(pokemonName)) return "legendary";
+  if (mythicals.includes(pokemonName)) return "mythical";
+  if (ultra_beasts.includes(pokemonName)) return "ultra_beast";
+  if (regional.includes(pokemonName)) return "regional";
+  if (event.includes(pokemonName)) return "event";
+
+  if (pokemon.includes(pokemonName)) return "regular";
 }
 
 module.exports = checkRarity;
