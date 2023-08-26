f1eaeae6e7e01a9d5f1e203c3c44e43667c10afb
Kyan <84374752+kyan0045@users.noreply.github.com>
2023-08-26 13:52:47 +0200

    v1.1.8 - Small variable changes

--- functions/checkRarity.js
+++ functions/checkRarity.js
@@ -3,18 +3,16 @@ async function checkRarity(pokemonName) {
   const legendaries = require("../data/legendary.json");
   const mythicals = require("../data/mythical.json");
   const ultra_beasts = require("../data/ultra-beast.json");
-  const regional = require("../data/regional.json");
-  const event = require("../data/event.json");
+  const regionals = require("../data/regional.json");
+  const events = require("../data/event.json");
 
-  if (!pokemon.includes(pokemonName)) {
-    throw new Error(`[PokeHint] Unable to identify the rarity of that pokemon (${pokemonName}).`);
-  }
+  if (!pokemon.includes(pokemonName)) throw new Error(`[PokeHint] Unable to identify the rarity of that pokemon (${pokemonName}).`);
   
   if (legendaries.includes(pokemonName)) return "Legendary";
   if (mythicals.includes(pokemonName)) return "Mythical";
   if (ultra_beasts.includes(pokemonName)) return "Ultra Beast";
-  if (regional.includes(pokemonName)) return "Regional";
-  if (event.includes(pokemonName)) return "Event";
+  if (regionals.includes(pokemonName)) return "Regional";
+  if (events.includes(pokemonName)) return "Event";
 
   if (pokemon.includes(pokemonName)) return "Regular";
 }
