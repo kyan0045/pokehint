01afe11aefc052f5b3f94978b67cd2f57021fcae
Kyan <84374752+kyan0045@users.noreply.github.com>
2024-04-01 01:06:11 +0200

    v1.3.7 - Added comments

--- functions/getImage.js
+++ functions/getImage.js
@@ -4,15 +4,18 @@ async function getImage(pokemon, shiny) {
 
   if (!pokemonName)
     throw new Error(
-      "[PokeHint] Could not find a pokemon name to get the image of."
+      `[PokeHint] No pokemon name specified to get the image of. (${pokemonName})`
     );
 
+  // Importing the images
   const images = require("../data/images/images.json");
   let image = images[pokemonName];
   if (!image)
     throw new Error(
       `[PokeHint] Unable to find an image for the Pokemon: ${pokemonName}`
     );
+
+  // Check whether to get the shiny image or not
   if (shinyState == true) {
     image = image.replace("images", "shiny");
   }
