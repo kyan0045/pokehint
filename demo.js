5ba14c706822e267f500b621d4d8cd499d00d87f
Kyan <84374752+kyan0045@users.noreply.github.com>
2024-04-01 01:11:05 +0200

    v1.3.7 - Updated the examples

--- demo.js
+++ demo.js
@@ -3,17 +3,26 @@ const solveHint = require("./functions/solveHint.js");
 const getImage = require("./functions/getImage.js");
 const checkRarity = require("./functions/checkRarity.js");
 
-async function test() {
+async function demo() {
+  // Solving hints
+  var hint = "The pokémon is Ch_r__n__r.";
+  console.log(solveHint(hint)); // Logs 'Charmander'.
+
+  // Checking the rarity
+  var pokemonName = "Moltres";
+  console.log(checkRarity(pokemonName)); // Logs Moltres' rarity: 'Legendary'.
+
+  // Converting a name to a different language
   console.log(
-    await getName({
-      name: "iron bundle",
+    getName({
+      name: "Charmander",
       language: "French",
       inputLanguage: "English",
     })
-  );
-  console.log(await getImage("rayquaza", true));
-  console.log(await solveHint("The pokémon is Ch_r__n__r."));
-  console.log(await checkRarity("rayquaza"));
+  ); // Logs the French name of Charmander: 'Salamèche'.
+
+  // Getting the Pokétwo image sprite of a specified pokemon
+  console.log(getImage("charmander", false)); // Logs 'https://cdn.poketwo.net/images/4.png'.
 }
 
-test();
+demo();
