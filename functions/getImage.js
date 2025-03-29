bea4d2f8518f7384306e881ebf1c0999b222d061
kyan0045 <84374752+kyan0045@users.noreply.github.com>
2025-03-29 01:09:09 +0100

    v1.4.13 - Add Gigantamax images

--- functions/getImage.js
+++ functions/getImage.js
@@ -2,34 +2,38 @@
 const images = require("../data/images/images.json");
 const forms = require("../data/images/forms.json");
 const events = require("../data/images/events.json");
+const gigantamaxImages = require("../data/images/gigantamax.json");
 
 /**
  * Retrieves the image URL for a given Pokémon.
  * @param {string} pokemon - The name of the Pokémon.
  * @param {boolean} [shiny=false] - Whether to get the shiny version of the Pokémon.
+ * @param {boolean} [gigantamax=false] - Whether to get the Gigantamax version of the Pokémon.
  * @returns {Promise<string>} The URL of the Pokémon image.
  * @throws {Error} If no Pokémon name is specified or if no image is found.
  */
-function getImage(pokemon, shiny = false) {
-  if (typeof pokemon !== 'string' || !pokemon.trim()) {
+function getImage(pokemon, shiny = false, gigantamax = false) {
+  if (typeof pokemon !== "string" || !pokemon.trim()) {
     throw new Error("[PokeHint] Invalid or empty Pokémon name specified.");
   }
-  
 
   const pokemonName = pokemon.toLowerCase();
 
   // Access the preloaded image data
   const imageOptions = [
+    gigantamax ? gigantamaxImages[`gigantamax ${pokemonName}`] : undefined,
     images[pokemonName],
     forms[pokemonName],
-    events[pokemonName]
+    events[pokemonName],
   ];
 
   // Find the first valid image
-  const image = imageOptions.find(img => img !== undefined);
+  const image = imageOptions.find((img) => img !== undefined);
 
   if (!image) {
-    console.log(`[PokeHint] Unable to find an image for the Pokémon: ${pokemon}`);
+    console.log(
+      `[PokeHint] Unable to find an image for the Pokémon: ${pokemon}`
+    );
     return null;
   }
 
